1f23302c8e6409f474ec00f96eec572d95e4fa8d
Huan <nguyenhuand@gmail.com>
2016-08-28 01:08:05 -0700

    logic to parse multiple columns

--- QBParse.py
+++ QBParse.py
@@ -4,10 +4,14 @@ import numpy as np
 def QBStats():
 
     df = pd.read_csv('../NFLDB/2015_QBvsDef.csv')
+    df1 = pd.read_csv('../NFLDB/2015_QB_Split.csv')
 
     #df1 = pd.DataFrame(df)
     #df.to_csv('../NFLDB/test.csv',index=False)
 
-    print(df.to_string(index=False))
+    print(df.loc[df['Team'].isin(['QB vs Falcons'])].to_string(index=False))
+    print(df1[(df1['Player'] == 'Cam Newton') & (df1['SPLIT'] == 'VS. ATL')])
+
+    #print(df.to_string(index=False))
 
     #print(df1)
